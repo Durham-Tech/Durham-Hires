d5ec9331930f6fbcebf0df60b7959883434cf91e
Jonathan Salmon <jonathan.salmon@hotmail.co.uk>
2017-05-02 10:39:39 +0100

    From last commit

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use View;
 use \App\content;
+use Image;
 
 class ContentController extends Controller
 {
@@ -29,10 +30,22 @@ class ContentController extends Controller
 
     public function savePage(Request $request)
     {
-        //TODO: Save images to file and check input valid.
         $page = content::where('page', $request->page)->firstOrFail();
         $content = strip_tags($request->content, '<p><a><span><h1><h2><h3><h4><h5><h6><li><ol><ul><br><div><blockquote><pre><font><table><tbody><thead><tr><td><th><img><iframe>');
+        $content = preg_replace_callback(
+            '/<img.+?src="(data:image\/[A-Za-z]+;base64,[^\"]+)".+?data-filename="[^\.]+\.([a-zA-Z]+)".+?>/',
+            function ($matches) {
+                $name = uniqid() . str_random(5) . '.' . $matches[2];
+                $img = Image::make($matches[1]);
+                $img->save('images/content/' . $name);
+                return str_replace($matches[1], '/images/content/' . $name, $matches[0]);
+            },
+            $content
+        );
         $page->content = $content;
         $page->save();
+
+
+
     }
 }
