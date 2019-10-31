e028280b2c7dc95e2160c004335369d6b94360c0
Jonathan Salmon <jonathan.salmon@hotmail.co.uk>
2019-10-31 23:59:16 +0000

    Made social login site independent

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'google' => [
         'client_id' =>  env('GOOGLE_APP_ID'),
         'client_secret' =>  env('GOOGLE_APP_SECRET'),
-        'redirect' => env('APP_URL') . '/auth/google/callback',
+        'redirect' => '/auth/google/callback',
     ],
 
 ];
